a777897b2e4d6ad6493e422258d45dd2d0a5edff
DANNY <est.djmoreno0201@uml.edu.ni>
2025-03-22 09:49:54 -0600

    anidados

--- if-anidados-switch.js
+++ if-anidados-switch.js
@@ -1,4 +1,9 @@
-const monto = Process.argv[2]
+const monto = process.argv[2]
 console.log ("El valor ingreesado es  ",monto)
-Process.exit(0)
+const numero=parseInt(monto)
+if(numero>500){
+    console.log("Has ingresado un valor> de 500")
+}else{
+    console.log("El numero es menor que 500")
+}
 
